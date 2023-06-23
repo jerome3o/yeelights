5ea86cc447c9bcf0c62f4ab41a2d5dccee147524
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-23 18:58:54 +1200

    Using environment variables in systemd units and turn_off script

--- turn_off.py
+++ turn_off.py
@@ -1,14 +1,19 @@
 import yeelight as yl
+import os
 
-_NAME = "Sam"
-_IP = "192.168.1.15"
+_LED_IP = os.environ.get("YEELIGHT_IP", None)
+_LAMP_IP = os.environ.get("YEELIGHT_LAMP_IP", None)
+
 
 def main():
-    b = yl.Bulb(_IP)
+    b = yl.Bulb(_LED_IP)
+    lamp = yl.Bulb(_LAMP_IP)
     b.turn_off()
+    lamp.turn_off()
 
 
 if __name__ == "__main__":
     import logging
+
     logging.basicConfig(level=logging.INFO)
     main()
